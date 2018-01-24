1bdace6ae7b78aa49576c3b299034cd52a3aff24
Ultraviolent Orange <35771720+ultraviolentorange@users.noreply.github.com>
2018-01-24 17:55:32 +0300

    Create setup_script.sh

---
+++ setup_script.sh
@@ -0,0 +1,33 @@
+# One script to rule(install) them all.
+
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# CLI Tools
+brew install wget
+brew install tree
+brew install htop
+brew install nmap
+brew install thefuck
+brew install the_silver_searcher
+
+# Browsers
+brew cask install firefox
+brew cask install google-chrome
+
+# VirtualBox Kali Linux
+brew cask install virtualbox
+wget https://images.offensive-security.com/virtual-images/kali-linux-2017.3-vbox-amd64.ova
+
+# Python Stuff
+brew install python3
+brew cask install pycharm-ce
+
+# Other Software
+brew cask install sublime-text
+brew cask install the-unarchiver
+brew cask install alfred
+brew install transmission
+brew cask install telegram-desktop
+brew cask install cheatsheet
+
