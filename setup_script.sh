eb5e16459467b6ec61202701253a6f05a6921893
Ultraviolent Orange <35771720+ultraviolentorange@users.noreply.github.com>
2018-02-05 01:33:00 +0300

    Update setup_script.sh

--- setup_script.sh
+++ setup_script.sh
@@ -6,8 +6,7 @@ brew install wget
 brew install tree
 brew install htop
 brew install nmap
-brew install thefuck
-brew install the_silver_searcher
+brew install the-silver-searcher
 brew install cmake
 
 # Frameworks & Libraries
@@ -17,10 +16,6 @@ brew install boost
 brew cask install firefox
 brew cask install google-chrome
 
-# VirtualBox Kali Linux
-brew cask install virtualbox
-wget https://images.offensive-security.com/virtual-images/kali-linux-2017.3-vbox-amd64.ova
-
 # Python Stuff
 brew install python3
 brew cask install pycharm-ce
@@ -29,6 +24,9 @@ brew cask install pycharm-ce
 brew cask install sublime-text
 brew cask install the-unarchiver
 brew cask install alfred
-brew install transmission
 brew cask install telegram
 brew cask install cheatsheet
+
+# VirtualBox Kali Linux
+brew cask install virtualbox
+wget https://images.offensive-security.com/virtual-images/kali-linux-2017.3-vbox-amd64.ova
